687d38cd083725d933e2478fc8d9a7023fa49314
unknown <madindoustephen8@gmail.com>
2025-09-08 03:49:19 +0300

    created django project

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "Hello World!")
+    return HttpResponse("<h1>Hello World!</h1>")
